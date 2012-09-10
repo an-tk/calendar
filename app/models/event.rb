33cbf338d5982ae24c56128f80fd925f46769c75
houston <supadm.fs@gmail.com>
2012-09-10 08:08:23 +0300

    event model refactoring

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,7 +15,6 @@ class Event < ActiveRecord::Base
   validates :start_at, :date => {:before_or_equal_to => :end_at, :message => 'must be before or equal to End at' }
   validates :start_at, :name, :end_at , :presence => true
   validates :repeat, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0, :less_than => REPEAT.length }
-  #validate :repeat_and_event_length
 
   scope :owner,  lambda { |owner|
     where(:user_id => owner) if owner=~/\d/
@@ -35,52 +34,9 @@ class Event < ActiveRecord::Base
       event_strips = [[nil] * (strip_end - strip_start + 1)]
 
       events.each do |event|
-        repeatable_events = []
         delta = (event.end_at.to_date - event.start_at.to_date).to_i
         cur_date = (event.start_at.to_date > strip_start) ? event.start_at : (strip_start - delta.days)
-        case event.repeat
-        when REPEAT.index(:none) then
-          repeatable_events << event
-        when REPEAT.index(:day) then
-          while cur_date <= strip_end do
-            dup = create_repeatable_dup(event, cur_date, delta)
-            repeatable_events << dup
-            cur_date = cur_date + 1.day
-          end
-        when REPEAT.index(:week) then
-          event_week_day = event.start_at.wday
-          while cur_date <= strip_end do
-            if cur_date.wday == event_week_day
-              dup = create_repeatable_dup(event, cur_date, delta)
-              repeatable_events << dup
-              cur_date = cur_date + 7.days
-            else
-              cur_date = cur_date + 1.day
-            end
-          end
-        when REPEAT.index(:month) then
-          event_month_day = event.start_at.day
-          while cur_date <= strip_end do
-            if cur_date.day == event_month_day
-              dup = create_repeatable_dup(event, cur_date, delta)
-              repeatable_events << dup
-              cur_date = cur_date + 28.days
-            else
-              cur_date = cur_date + 1.day
-            end
-          end
-        when REPEAT.index(:year) then
-          event_year_day = event.start_at.yday
-          while cur_date <= strip_end do
-            if cur_date.yday == event_year_day
-              dup = create_repeatable_dup(event, cur_date, delta)
-              repeatable_events << dup
-              break
-            else
-              cur_date = cur_date + 1.day
-            end
-          end
-        end
+        repeatable_events = event.generate_repeatable_events(strip_end, cur_date, delta)
         repeatable_events.each do |event|
           cur_date = event.start_at.to_date
           end_date = event.end_at.to_date
@@ -110,26 +66,84 @@ class Event < ActiveRecord::Base
       event_strips
     end
 
-    def create_repeatable_dup(event, cur_date, delta)
-      dup = event.dup
-      dup.id = event.id
-      dup.start_at = cur_date
-      dup.end_at = cur_date + delta.days
-      dup
+  end
+
+  def create_repeatable_dup cur_date, delta
+    dup = self.dup
+    dup.id = self.id
+    dup.start_at = cur_date
+    dup.end_at = cur_date + delta.days
+    dup
+  end
+
+  def generate_repeatable_events strip_end, cur_date, delta
+    case self.repeat
+      when REPEAT.index(:none) then
+        [self]
+      when REPEAT.index(:day) then
+        generate_daily_events(strip_end, cur_date, delta)
+      when REPEAT.index(:week) then
+        generate_weekly_events(strip_end, cur_date, delta)
+      when REPEAT.index(:month) then
+        generate_monthly_events(strip_end, cur_date, delta)
+      when REPEAT.index(:year) then
+        generate_yearly_events(strip_end, cur_date, delta)
     end
   end
 
-  private
-
-  def repeat_and_event_length
-    rez = case repeat
-          when 1 then (end_at.to_date - start_at.to_date) == 0 ? true : false
-          when 2 then (end_at.to_date - start_at.to_date) < 7 ? true : false
-          when 3 then (end_at.to_date - start_at.to_date) < 30 ? true : false
-          when 4 then (end_at.to_date - start_at.to_date) < 365 ? true : false
-          else true
-          end
-    errors.add(:repeat, "of event with that length can't be so often") unless rez
+  def generate_daily_events strip_end, cur_date, delta
+    events = []
+    while cur_date <= strip_end do
+      dup = create_repeatable_dup(cur_date, delta)
+      events << dup
+      cur_date = cur_date + 1.day
+    end
+    events
   end
+  def generate_weekly_events(strip_end, cur_date, delta)
+    events = []
+    event_week_day = self.start_at.wday
+    while cur_date <= strip_end do
+      if cur_date.wday == event_week_day
+        dup = create_repeatable_dup(cur_date, delta)
+        events << dup
+        cur_date = cur_date + 7.days
+      else
+        cur_date = cur_date + 1.day
+      end
+    end
+    events
+  end
+
+  def generate_monthly_events(strip_end, cur_date, delta)
+    events = []
+    event_month_day = self.start_at.day
+    while cur_date <= strip_end do
+      if cur_date.day == event_month_day
+        dup = create_repeatable_dup(cur_date, delta)
+        events << dup
+        cur_date = cur_date + 28.days
+      else
+        cur_date = cur_date + 1.day
+      end
+    end
+    events
+  end
+
+  def generate_yearly_events(strip_end, cur_date, delta)
+    events = []
+    event_year_day = self.start_at.yday
+    while cur_date <= strip_end do
+      if cur_date.yday == event_year_day
+        dup = create_repeatable_dup(cur_date, delta)
+        events << dup
+        break
+      else
+        cur_date = cur_date + 1.day
+      end
+    end
+    events
+  end
+
 
 end
